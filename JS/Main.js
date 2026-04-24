c7c51feeab15d05b4707b6dbeb8ed6c111ee8d34
Kaileb Becker <kjbecke3@asu.edu>
2026-04-24 01:00:51 -0700

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -1,2 +1,15 @@
 // future shared features (menu toggle, etc.)
 console.log("Main JS loaded");
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle("active");
+};
+
+// close menu when clicking a link
+document.querySelectorAll(".navbar a").forEach(link => {
+    link.onclick = () => {
+        navbar.classList.remove("active");
+    };
+});
